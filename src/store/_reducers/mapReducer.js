ecf42a28428c45acad1d7a6d487ac0a5b9e3f048
datta9 <datta.yh@gmail.com>
2022-11-04 18:02:56 +0530

    Bug Fix

--- src/store/_reducers/mapReducer.js
+++ src/store/_reducers/mapReducer.js
@@ -11,15 +11,15 @@ const mapReducer = (state = initialState, action) => {
             const options = {units: 'meters'}
 
             if(state.markerCoordinates.length === 1) {
-              let from = point(Object.values(state.markerCoordinates[0]))
-              let to = point(Object.values(coords))
-              let distanceResult = distance(from, to, options).toFixed([2])
-              coords.distance = distanceResult
+                let from = point([state.markerCoordinates[0].lng, state.markerCoordinates[0].lat])
+                let to = point([coords.lng, coords.lat])
+                let distanceResult = distance(from, to, options).toFixed([2])
+                coords.distance = distanceResult
             } else if(state.markerCoordinates.length !== 0) {
-              let from = point(Object.values(state.markerCoordinates[state.markerCoordinates.length - 2]).slice(0, 2))
-              let to = point(Object.values(coords))
-              let distanceResult = distance(from, to, options).toFixed([2])
-              coords.distance = distanceResult
+                let from = point([state.markerCoordinates[state.markerCoordinates.length - 1].lng, state.markerCoordinates[state.markerCoordinates.length - 1].lat])
+                let to = point([coords.lng, coords.lat])
+                let distanceResult = distance(from, to, options).toFixed([2])
+                coords.distance = distanceResult
             }
             return {
                 ...state,
